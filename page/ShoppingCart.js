f16b18e2c671546f9a1fbd4cc75e9487f983d208
Shraddha <shraddhasharma6991@gmail.com>
2026-02-05 09:13:10 +0530

    few small updates

--- page/ShoppingCart.js
+++ page/ShoppingCart.js
@@ -3,25 +3,38 @@ const AddCart= require('../page/AddCart.js');
 
 class ShoppingCart extends AddCart
 {
+     num;
       constructor(driver)
     {
         super(driver)
-        this.driver=driver  
+        this.driver=driver 
     }
     async SelectItemShop()
     {
-        this.removeitem = this.driver.wait(until.elementLocated(By.xpath("//input[@name='removefromcart']")))
+        this.removeitem = this.driver.wait(until.elementLocated(By.xpath("//input[@value='6287895']")))
         await this.driver.wait(until.elementIsVisible(this.removeitem))
         await this.removeitem.click()
     }
     async ChangeQuantity(num)
     {
-        this.quantity= this.driver.wait(until.elementLocated(By.xpath("//input[@name='removefromcart']")))
+        this.quantity= this.driver.wait(until.elementLocated(By.xpath("//input[@name='itemquantity6287895']")))
         await this.driver.wait(until.elementIsVisible(this.quantity))
         await this.quantity.clear()
+        this.num =num
         const value = num
         await this.quantity.sendKeys(value)
-        const total = num*10
+    }
+
+    async CalculateChange()
+    {
+        const price = this.driver.wait(untilelementLocated(By.css('span.product-unit-price')))
+        await this.driver.wait(until.elementIsVisisble(this.price))
+        const value_price = await this.price.getText()
+        console.log(value_price)
+        const integer_value = driver.execute_script(
+        "return parseInt(parseFloat(arguments[0]));", value_price)
+         const total = this.num*integer_value
+         console.log("The total" , total)
     }
 }
 module.exports= ShoppingCart
--- step_definitions/addtocart.js
+++ step_definitions/addtocart.js
@@ -7,33 +7,44 @@ const LoginPage= require('../page/LoginPage.js')
 const ShoppingCart = require('../page/ShoppingCart.js')
 
 let driver
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 const assert =require("assert")
 Given('user login to website', async function()
 {
 this.driver = await new Builder().forBrowser('chrome').build();
  const base = new BasePage(this.driver)
  await base.goToUrl('https://demowebshop.tricentis.com/login');
+ const logpage = new LoginPage(this.driver)
+ await logpage.LoginClickButton()
+ await logpage.EmailValue('shraddhasharma2603@gmail.com')
+ await logpage.PasswordValue('Shraddha@26')
+ await logpage.ClickOnLoginButton()
 })
 When('user selects the item', async function()
 {
  const cartadded= new AddCart(driver)
- await this.cartadded.SelectItemShop()
- await this.cartadded.AddToCart()
+ await cartadded.SelectItems()
+ await cartadded.AddToCart()
 })
 Then('user navigate to add cart', async function()
 {
  const cartcheck= new AddCart(this.driver)
- await this.cartcheck.MoveToShoppingCart()
+ await cartcheck.MoveToShoppingCart()
 })
 
 When('user goes to shopping cart',async function()
 {
+ // console.log("Driver:-", this.driver)
+ const cartcheck= new AddCart(this.driver)
+ await cartcheck.MoveToShoppingCart()
  const quantity = new ShoppingCart(this.driver)
-  await this.quantity.SelectItemShop()
+ //console.log('Methods:', Object.getOwnPropertyNames(quantity));  // Lists prototype 
+  await quantity.SelectItemShop()
 })
 Then('user can change quantity', async function()
 {
  const number= new ShoppingCart(this.driver)
- await this.number.ChangeQuantity(3)
-
+ await number.ChangeQuantity(5)
+ await sleep(3000)
+await number.CalculateChange()
 })
