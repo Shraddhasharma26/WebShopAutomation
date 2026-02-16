847adf22962dfe91828920fb604d1fd4670c5d94
=shraddhasharma26 <shraddhasharma6991@gmail.com>
2026-02-16 17:53:32 +0530

    Added page for display feature

--- page/DisplayFeature.js
+++ page/DisplayFeature.js
@@ -10,7 +10,38 @@ class DisplayFeature extends BasePage
     {
         super(driver)
         this.driver=driver
+        this.DisplayNumber=null
     }
 
- async 
+ async NavigationOption()
+ {
+    const shoes = await this.driver.findElements(By.xpath("//a[@href ='/apparel-shoes']"))
+    const element = shoes[3]
+    await this.driver.element.click()
+    const display = await this.driver.findElement(By.id('products-pagesize'))
+    await this.driver.display.click()
+    this.DisplayNumber = this.driver.wait(until.elementLocated(By.xpath("//select//option[@value='https://demowebshop.tricentis.com/apparel-shoes?pagesize=4']")))
+    await this.driver.wait(until.elementIsVisible(this.DisplayNumber))
+    await this.driver.DisplayNumber.click()
+    //const actions = driver.actions({ async: true });
+    //await actions.move({ origin: actions }).perform()
+ }
+ async assertNumuberOfItem()
+ {
+    let count =0
+    const countquantity = this.driver.findElements(By.css('div.item-box'))
+    for (let i=0; i<countquantity.length; i++)
+    {
+        count=count+1
+    }
+    console.log("The total count is ", count)
+    try
+    {
+    assert.strictEqual(this.DisplayNumber, count,"Test case passed")
+    }
+    catch (error) 
+        {
+            console.error("An error occurred:", error);
+        }
+ }
 }
---
+++ step_definitions/QuantityDisplayed.js
@@ -0,0 +1,26 @@
+const {Given, When, Then,setDefaultTimeout}= require("@cucumber/cucumber")
+const {By ,Builder, until, Key}= require("selenium-webdriver")
+
+const BasePage = require('../page/BasePage.js');
+const LoginPage= require('../page/LoginPage.js')
+const DisplayFeature = require('../page/DisplayFeature.js')
+
+Given('user launch the website and login to the page',async function()
+{
+ const 
+})
+
+Given('user selects any naviagtion option', async function()
+{
+
+})
+
+When('user changes the quantity per page', async function()
+{
+
+})
+
+Then('number of item displayed on that page is equal to the number of quantity selected',async function()
+{
+
+})
